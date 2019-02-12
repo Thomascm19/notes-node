15fd1d1dcfcd1e7fa1290901a8ac278a8e0435f5
Thomas <thomascaycedo19@outlook.com>
2019-02-11 21:07:57 -0500

    Se Actualiza la logica

--- logica/logica.js
+++ logica/logica.js
@@ -37,6 +37,28 @@ const crear = (descripcion) => {
     return tarea; 
 }
 
+const getListado = ()=>{
+    cargardb();
+    return listado
+}
+
+const actualizar = (descripcion, completado= true)=>{
+    cargardb();
+    let index = listado.findindex(i=>{
+        return i.descripcion === descripcion});
+
+        if(index >=0){
+            listado[index].completado = completado;
+            guardarjson();
+            return true;
+        }else{
+            false;
+        }
+    
+}
+
 module.exports = {
-    crear
+    crear,
+    getListado, 
+    actualizar
 }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const argv = require('./config/config').argv;
-const tareas = require('./logica/logica')
+const tareas = require('./logica/logica');
+const colors = require('colors');
 //const argv = require('yargs').argv;
 
 console.log(argv);
@@ -11,9 +12,19 @@ switch(comando){
     let tarea = tareas.crear(argv.descripcion)
         console.log(tarea);
     break;
+
     case 'listar':
-        console.log("Se muestran todas las listas")
+
+        let listado = tareas.getListado();
+
+        for (let tarea of listado){
+            console.log("=====Tareas Pendientes=====".green);
+            console.log(tarea.descripcion);
+            console.log("Estado: ", tarea.completado)
+            console.log("===========================".green);
+        }
     break;
+
     case 'actualizar':
         console.log("Se completa la lista pendiente")
     break;
